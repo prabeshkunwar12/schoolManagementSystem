b247c6e9e7e1feeafa9b571298992f1386efcdfe
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2023-11-21 17:59:24 -0400

    First Commit

---
+++ src/main/java/com/schoolManagement/schoolManagement/SchoolManagementApplication.java
@@ -0,0 +1,13 @@
+package com.schoolManagement.schoolManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schoolManagement/schoolManagement/SchoolManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolManagement.schoolManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
